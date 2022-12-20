b1b81b234fd7bc3cfc9d145f6191dd266dad6c2f
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-12-20 16:32:31 +0100

    starting project

---
+++ script.js
@@ -0,0 +1,12 @@
+// targeted elements by id and assign into variables ( we’ll use these variables later in tutorial to print calculation text and result text )
+
+/* function to insert number onclick */
+
+// targeting clear button by id and assign a blank value to the calculation and 0 to result on click event listener
+
+/* on click event listener targeting equal button by id then 
+- assign the calculation to the exp variable (we'll use this variable to evaluates calculation using eval method)
+- assign exp variable using the eval method and assign to the result to result screen 
+- assign the output of the result to the calculation to print result to calculation screen too  */
+
+// target back key button by id and assign value of calculation to exp variable then assign calculation by substring exp variable from 0 to (length of exp -1 )
