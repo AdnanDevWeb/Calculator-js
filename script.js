11237a715d86aa153e084056b74f022b5d5def88
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-12-23 19:27:50 +0100

    js finish

--- script.js
+++ script.js
@@ -1,9 +1,123 @@
+class Calculator{
+    constructor(previousOperandTextlElement, currentOperandTextElement){
+        this.previousOperandTextlElement = previousOperandTextlElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
+    }
 
-// targeting clear button by id and assign a blank value to the calculation and 0 to result on click event listener
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
+    }
+    appendNumber(num){
+        if(num === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + num.toString()
+    }
+    chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = ''
+    }
 
-/* on click event listener targeting equal button by id then 
-- assign the calculation to the exp variable (we'll use this variable to evaluates calculation using eval method)
-- assign exp variable using the eval method and assign to the result to result screen 
-- assign the output of the result to the calculation to print result to calculation screen too  */
 
-// target back key button by id and assign value of calculation to exp variable then assign calculation by substring exp variable from 0 to (length of exp -1 )
+    compute(){
+        let computation;
+        let prev = parseFloat(this.previousOperand)
+        let current = parseFloat(this.currentOperand)
+
+        if(isNaN(prev) || isNaN(current)) return
+
+        switch(this.operation){
+            case '+':
+                computation = prev + current;
+                break
+
+            case '*':
+                computation = prev * current
+                break
+
+            case '-':
+                computation = prev - current
+                break
+
+            case '/':
+                computation = prev / current
+                break
+            
+            default:
+                return
+        }
+
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = ''
+
+    }
+    getDisplayNumnber(number){
+        const floatNumber = parseFloat(number)
+        if(isNaN(floatNumber)) return ''
+        return floatNumber.toLocaleString('en')
+    }
+
+    update(){
+        this.currentOperandTextElement.innerText = this.getDisplayNumnber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextlElement.innerText = `${this.previousOperand} ${this.operation}`
+
+        }
+
+    }
+}
+
+
+
+
+
+
+const del = document.querySelector('[data-del]')
+const ac = document.querySelector('[data-ac]')
+const equalsButton = document.querySelector('[data-equals]')
+const numberButtons = document.querySelectorAll('[data-num]')
+const operationsButtons = document.querySelectorAll('[data-operation]')
+
+const previousOperandTextlElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextlElement,currentOperandTextElement)
+
+// addEvent
+ac.addEventListener('click' , ()=>{
+    calculator.clear()
+    calculator.update()
+})
+del.addEventListener('click' , ()=>{
+    calculator.delete()
+    calculator.update()
+})
+numberButtons.forEach(number =>{
+    number.addEventListener('click' , ()=>{
+        calculator.appendNumber(number.innerText)
+        calculator.update()
+    })
+})
+
+operationsButtons.forEach(operation =>{
+    operation.addEventListener('click' , ()=>{
+        calculator.chooseOperation(operation.innerText)
+        calculator.update()
+    })
+})
+
+
+equalsButton.addEventListener('click' , button =>{
+    calculator.compute();
+    calculator.update()
+})
